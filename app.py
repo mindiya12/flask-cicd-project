c836a1ea5400481616da2b571121a3276af9faea
Thisan Mindiya Kulathilake <139038281+mindiya12@users.noreply.github.com>
2025-11-16 10:38:36 +0530

    test2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hello there, from Flask App!</h1><p>Deployment successful.</p>'
+    return '<h1>Hello from Flask App!</h1><p>Deployment successful.</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
